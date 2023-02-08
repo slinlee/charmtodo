3554b58758a6f5e8ef0a075c3dc8d7c84e80f5df
Slin Lee <slin@slinlee.com>
2023-02-08 15:44:20 -0800

    Add code for bubbletea hello world

--- main.go
+++ main.go
@@ -1,9 +1,88 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
+)
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices: []string{"buy a", "buy b", "buy c"},
+
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+
+	s := "What should we buy at the market?\n\n"
+
+	// Iterate over our choices
+
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UY for rendering
+
+	return s
+}
 
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println(quote.Go())
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
