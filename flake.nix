1afab9e40e9ed32501b6a0d80ae3e37140ca16c2
Slin Lee <slin@slinlee.com>
2023-02-08 15:07:14 -0800

    Add go to the project

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.default = pkgs.mkShell {
-        packages = [ pkgs.bashInteractive ];
+        packages = [ pkgs.go];
       };
     });
 }
