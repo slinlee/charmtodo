a032f62df15ce292414b58c1024c59ca7b35f1ba
Slin Lee <slin@slinlee.com>
2023-02-08 15:09:16 -0800

    Add go debugger

--- flake.nix
+++ flake.nix
@@ -4,11 +4,16 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = [ pkgs.go];
-      };
-    });
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gopls
+          ];
+        };
+      });
 }
