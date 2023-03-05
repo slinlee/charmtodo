f6e5a6520359d134149e2adebe8a72f9f0ab7a87
Slin Lee <slin@slinlee.com>
2023-03-05 13:19:54 -0800

    Only add to display data if in the view range

--- main.go
+++ main.go
@@ -136,7 +136,10 @@ func getDateIndex(date time.Time) (int, int) {
 func parseCalToView(calData []CalDataPoint) {
 	for _, v := range calData {
 		x, y := getDateIndex(v.Date)
-		viewData[x][y].actual += v.Value
+		// Check if in range
+		if x > -1 && y > -1 {
+			viewData[x][y].actual += v.Value
+		}
 	}
 	normalizeViewData()
 }
